aba49e3c87fcc819693d8d78e2c21fe0115806ec
Anna <cameaann@gmail.com>
2024-08-14 23:35:30 +0300

    creating skeleton from Spring initializr

---
+++ src/main/java/com/example/shareholders_app_backend/ShareholdersAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareholders_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareholdersAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareholdersAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shareholders_app_backend/ShareholdersAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareholders_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareholdersAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
