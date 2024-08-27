bd1db117856bfe34b9171d14717f70045b61c3dd
Alexei <>
2024-08-27 12:33:06 +0300

    Updated Shareholder entity class with new fields

--- src/main/java/server/shareholders_app_backend/model/Shareholder.java
+++ src/main/java/server/shareholders_app_backend/model/Shareholder.java
@@ -15,24 +15,23 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Shareholder {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String name; // Shareholder's name
 
-    private Integer maara; // Number of shares
+    private Integer numberOfShares; // Number of shares owned
 
-    private Double omistusPercentage; // Ownership percentage
+    private Double ownershipPercentage; // Ownership percentage
 
-    private String hetuYtunnus; // Personal ID or company identifier
+    private String personalIdOrCompanyId; // Personal ID or company identifier
 
-    private String kotipaikka; // Place of residence or headquarters
+    private String residenceOrHeadquarters; // Place of residence or headquarters
 
-    private String yhteystiedot; // Contact information
+    private String contactInformation; // Contact information
 
-    private String tilinro; // Bank account number
+    private String bankAccountNumber; // Bank account number
 
     @OneToMany(mappedBy = "shareholder", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
