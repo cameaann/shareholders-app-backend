3d1e584a1177d1603a00172c0418083165c6e897
alex <againullin8@gmail.com>
2024-08-30 12:25:53 +0300

    Percentage_calculate

---
+++ src/main/java/server/shareholders_app_backend/config/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package server.shareholders_app_backend.config;
+
+public final class ApplicationConstants {
+    private ApplicationConstants() {
+
+    }
+
+    public static final int TOTAL_SHARES_IN_COMPANY = 4070921;
+}
