dd9daecfcb0c34597a403938bd3a0276697ce048
alex <againullin8@gmail.com>
2024-09-04 23:14:46 +0300

    check-id-existence

--- src/main/java/server/shareholders_app_backend/repository/ShareholderRepository.java
+++ src/main/java/server/shareholders_app_backend/repository/ShareholderRepository.java
@@ -1,7 +1,12 @@
 package server.shareholders_app_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import server.shareholders_app_backend.model.Shareholder;
 
 public interface ShareholderRepository extends JpaRepository<Shareholder, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(s) > 0 THEN true ELSE false END FROM Shareholder s WHERE s.personalIdOrCompanyId = :personalIdOrCompanyId")
+    boolean existsByPersonalIdOrCompanyId(@Param("personalIdOrCompanyId") String personalIdOrCompanyId);
 }
