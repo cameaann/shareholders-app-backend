f48ec1d6601d41aed4b93e85ba078468c36827bd
Alexei <>
2024-10-01 15:41:25 +0300

    bug fix ok

--- src/main/java/server/shareholders_app_backend/controller/TransferController.java
+++ src/main/java/server/shareholders_app_backend/controller/TransferController.java
@@ -1,20 +1,22 @@
-package server.shareholders_app_backend.controller;
+// package server.shareholders_app_backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import server.shareholders_app_backend.dto.TransferRequestDto;
-import server.shareholders_app_backend.service.TransferService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+// import server.shareholders_app_backend.dto.TransferRequestDto;
+// import server.shareholders_app_backend.service.TransferService;
 
-@RestController
-@RequestMapping("/api/transfer")
-public class TransferController {
+// @RestController
+// @RequestMapping("/api/transfer")
+// public class TransferController {
 
-    @Autowired
-    private TransferService transferService;
+// @Autowired
+// private TransferService transferService;
 
-    @PostMapping
-    public TransferRequestDto handleTransfer(@RequestBody TransferRequestDto transferRequestDto) {
-        TransferRequestDto populatedDto = transferService.populateShareholderDetails(transferRequestDto);
-        return populatedDto; // Return the populated DTO
-    }
-}
+// @PostMapping
+// public TransferRequestDto handleTransfer(@RequestBody TransferRequestDto
+// transferRequestDto) {
+// TransferRequestDto populatedDto =
+// transferService.populateShareholderDetails(transferRequestDto);
+// return populatedDto; // Return the populated DTO
+// }
+// }
